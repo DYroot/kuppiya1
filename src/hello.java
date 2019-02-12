d618fbcd8277baf992b5ea6097265c5a40679e18
DYroot <dileepayasas@gmail.com>
2019-02-12 07:27:15 +0530

    hello world file

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String args[]) // this is the main method
+    {
+        System.out.println("Hello world\n");
+        System.out.print("java is a vary protable");
+        System.out.println("and can run in different types of computers.");
+        System.out.println("This is the next line!!");
+    }
+
+}
